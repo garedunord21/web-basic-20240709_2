1da6b33338d4d6c6aacd28dee047536b089c445a
garedunord21 <garedunord21@gmail.com>
2024-08-08 16:57:59 +0900

    2024.08.08 16:50

--- ES6/4.parameter_handling/parameter_handling.js
+++ ES6/4.parameter_handling/parameter_handling.js
@@ -30,7 +30,7 @@ console.log('========================================');
 /*
     spread 연산자 : 
     - ...을 사용하여 대상의 요소를 하나씩 분리
-    - 연산자의 피연산자는 반드시 반복 가능한 객체(배열, 객체, 문자열, ...) 이어야 함
+    - 연산자의 피연사자는 반드시 반복 가능한 객체(배열, 객체, 문자열, ...) 이어야 함
 */
 const numbers = [1, 2, 3, 4];
 console.log(numbers);
@@ -51,7 +51,46 @@ console.log('========================================');
 
 // 배열 연결에 사용
 const numbers2 = [5, 6, 7, 8];
-let newNumbers2 = [ ...numbers, ...numbers2 ]; // => [1, 2, 3, 4]
+
+let newNumbers2 = [ ...numbers, ...numbers2 ];  // => [1, 2, 3, 4, 5, 6, 7, 8]
 console.log(newNumbers2);
-newNumbers2 = [ ...numbers2, ...numbers ];
+newNumbers2 = [ ...numbers2, ...numbers ];  // => [5, 6, 7, 8, 1, 2, 3, 4]
 console.log(newNumbers2);
+
+console.log('========================================');
+
+// 배열 요소 추가에 사용
+newNumbers2 = [ ...newNumbers2, 9, 10 ];
+console.log(newNumbers2);
+
+console.log('========================================');
+
+// 객체 복사, 연결, 요소 추가에 사용
+const king = {
+    name: '이성계',
+    tombName: '태조'
+};
+
+let newKing = { ...king };
+newKing.name = '이방과';
+console.log(king);
+console.log(newKing);
+
+const kingInfo = {
+    address: '서울특별시',
+    country: '고려'
+};
+
+newKing = { ...king, ...kingInfo };
+console.log(newKing);
+newKing = { ...kingInfo, ...king };
+console.log(newKing);
+
+newKing = { ...king, birth: '1335-11-04' };
+console.log(newKing);
+
+newKing = { ...king, name: '이단', birth: '1335-11-04' };
+console.log(newKing);
+
+newKing = { name: '이단', ...king, birth: '1335-11-04' };
+console.log(newKing);
